5a1a1098d25ac194f7ddd3e416e169513ae05853
mustafabawany <k191273@nu.edu.pk>
2022-07-24 17:26:56 +0500

    Firebase Implemented

--- Web App/AutoFiller/main.py
+++ Web App/AutoFiller/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File , UploadFile
 from fastapi.templating import Jinja2Templates 
 from fastapi.staticfiles import StaticFiles
+from pdf2image import convert_from_path
+import cv2
+import matplotlib.pyplot as plt
 
 app = FastAPI()
 templates = Jinja2Templates(directory= 'templates')
@@ -17,3 +20,21 @@ def getAboutUs(request : Request):
 @app.get("/auto-complete")
 def getAutocomplete(request : Request):
     return templates.TemplateResponse("autocomplete.html" , {"request":request})
+
+@app.get("/auto-complete/{resume_id}")
+async def ProcessResume(resume_id: str , request : Request):
+    return templates.TemplateResponse("parsedResume.html" , {"request" : request})
+
+@app.post("/auto-complete")    
+async def getPDF(resume : UploadFile = File(...)):
+    return {"resume" : resume}
+
+@app.get("/success")
+def successPage(request : Request):
+    return {"Success" : "Yayyy!"}
+
+@app.post("/success")
+def getInfo(name: str , email : str , contact : str):
+    return {"Success" : "Yayyy!!"}
+
+
---
+++ Web App/AutoFiller/static/index.js
@@ -0,0 +1,27 @@
+document.getElementById('submitButton').addEventListener("click" , (e) => {
+	let file = document.getElementById('resume').files[0]
+	console.log(file)
+	const storageRef = firebase.storage().ref("Resume/" + file.name)
+	const task = storageRef.put(file)
+
+	task.on(
+		"state change" ,
+		function progress(progress){
+			console.log(progress.bytesTransferred / progress.totalBytes *100)
+		},  
+		function error(err){
+			console.log('There was An Err ' + err)
+		},
+		function completed(){
+			storageRef.getDownloadURL()
+			.then(url => {
+				const newURL = new URL(url)
+				let tempToken = newURL.search				
+				let index1 = tempToken.indexOf('&' , 0);
+				let index2 = tempToken.length
+				const token_id = tempToken.slice(index1 + 1, index2)
+				location.href = 'http://localhost:8000/auto-complete/' + token_id
+			})
+		}
+	)
+})
